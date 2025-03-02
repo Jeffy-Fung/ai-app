7f2489c953d401e8bec20e8c87183604ed4571eb
Jeffy-Fung <jeffyfung0414@gmail.com>
2025-03-02 15:55:25 +0800

    feat: integrate corrective retrieval workflow into graph

--- graph.py
+++ graph.py
@@ -1,6 +1,7 @@
 from typing import Optional, TypedDict
 from langgraph.graph import StateGraph, START, END
 from node import Node
+from corrective_retrieval_node import CorrectiveRetrievalNode
 from state import State
 from dotenv import load_dotenv
 
@@ -19,13 +20,19 @@ def is_conversation_long(state: State):
 class Graph:
   def __init__(self):
     graph_builder = StateGraph(State, ConfigSchema)
-    node = Node()
 
+    node = Node()
     graph_builder.add_node("extract_recent_chat_history", node.extract_recent_chat_history)
     graph_builder.add_node("generate_chat_summary", node.generate_chat_summary)
     graph_builder.add_node("generate_search_query", node.generate_search_query)
     graph_builder.add_node("retrieve_documents", node.retrieve_documents)
     graph_builder.add_node("generate_response", node.generate_response)
+    
+    corrective_retrieval_node = CorrectiveRetrievalNode()
+    graph_builder.add_node("retrieval_grader", corrective_retrieval_node.retrieval_grader)
+    graph_builder.add_node("remove_erroneous_retrievals", corrective_retrieval_node.remove_erroneous_retrievals)
+    graph_builder.add_node("define_web_search_query", corrective_retrieval_node.define_web_search_query)
+    graph_builder.add_node("support_documents_with_web_search", corrective_retrieval_node.support_documents_with_web_search)
 
     graph_builder.add_edge(START, "extract_recent_chat_history")
     graph_builder.add_conditional_edges(
@@ -42,10 +49,14 @@ class Graph:
       "retrieve_documents",
       is_documents_found,
       {
-        True: "generate_response",
+        True: "retrieval_grader",
         False: END,
       }
     )
+    graph_builder.add_edge("retrieval_grader", "remove_erroneous_retrievals")
+    graph_builder.add_edge("remove_erroneous_retrievals", "define_web_search_query")
+    graph_builder.add_edge("define_web_search_query", "support_documents_with_web_search")
+    graph_builder.add_edge("support_documents_with_web_search", "generate_response")
     graph_builder.add_edge("generate_response", END)
     self.graph = graph_builder.compile()
 
